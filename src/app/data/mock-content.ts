5d08901371ce9d5452665c4f57d17220bae65782
JavaNerd1995 <w0660242@myscc.ca>
2023-02-10 16:09:01 -0500

    added css

--- src/app/data/mock-content.ts
+++ src/app/data/mock-content.ts
@@ -17,23 +17,23 @@ export const listOfGames: IContent[] = [{
     type: ''
   },{
     id: 1026,
-    imgSrc: 'https://hindmoviez.app/wp-content/uploads/2023/01/The-Last-of-Us-Season-1-English.jpg',
-    description: '<p>This is the body of the content</p>',
-    title: 'the last of us',
-    author: 'Sony play-station studios',
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/af/The_Division_2_art.jpg/220px-The_Division_2_art.jpg',
+    description: "<p>Tom Clancy's The Division 2 is an online-only action role-playing video game developed by Massive Entertainment and published by Ubisoft. The sequel to Tom Clancy's The Division (2016), it is set in a near-future Washington, D.C.</p>",
+    title: 'Tom Clancys the division',
+    author: 'Massive entertainment',
     type: ''
   },{
     id: 1027,
-    imgSrc: 'https://hindmoviez.app/wp-content/uploads/2023/01/The-Last-of-Us-Season-1-English.jpg',
-    description: '<p>This is the body of the content</p>',
-    title: 'the last of us',
-    author: 'Sony play-station studios',
+    imgSrc: 'https://cdn.supersoluce.com/file/docs/docid_50f633a58f152fee45000051/elemid_4ee9d6ec0a2fe93f0e00000c/the-witcher-3-wild-hunt-pc.jpg',
+    description: '<p>The Witcher 3: Wild Hunt is a 2015 action role-playing game developed and published by CD Projekt. It is the sequel to the 2011 game The Witcher 2: Assassins of Kings and the third game in The Witcher video game series.</p>',
+    title: 'The Witcher 3',
+    author: 'CD Projekt Red',
     type: ''
   },{
     id: 1028,
-    imgSrc: 'https://hindmoviez.app/wp-content/uploads/2023/01/The-Last-of-Us-Season-1-English.jpg',
-    description: '<p>This is the body of the content</p>',
-    title: 'the last of us',
+    imgSrc: 'https://i.redd.it/2afuemjc8ek31.jpg',
+    description: '<p>Cyberpunk 2077 is a 2020 action role-playing video game developed by CD Projekt Red and published by CD Projekt. Set in Night City, an open world set in the Cyberpunk universe, players assume the role of a customisable mercenary known as V, who can acquire skills in hacking and machinery with options for melee and ranged combat.</p>',
+    title: 'CyberPunk 2077',
     author: 'Sony play-station studios',
     type: ''
   }
