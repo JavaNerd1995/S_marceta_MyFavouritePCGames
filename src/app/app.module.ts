46a13861ffa61e4afbaf397960d72b74c4338d8e
JavaNerd1995 <w0660242@myscc.ca>
2023-02-13 18:16:01 -0500

    updated assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListItemComponent } from './content-list-item/content-list-item.component';
-import { AdvancedIContentComponent } from './advanced-icontent/advanced-icontent.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
+import { SearchComponent } from './search/search.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentListItemComponent,
-    AdvancedIContentComponent
+    ContentDetailComponent,
+    SearchComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
